d2772027a52d1d524c8f21f9bed6ba13262d7000
kotooo8080 <71766913+kotooo8080@users.noreply.github.com>
2021-03-30 21:20:32 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {11, 22, 33, 44, 55, 66, 77, 88};
+           
+            
+            
+        }
+
+    }
+}
